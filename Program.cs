6ee47d2a611affc7559ddfe74db899cdba2c1961
Jakub Vitásek <jakub.vitasek@educhem.cz>
2022-04-19 13:23:26 +0200

    Přidejte soubory projektu.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("zadejte  první číslo");
+string cislo1 = Console.ReadLine();
+int cislo11 = Convert.ToInt32(cislo1);
+Console.Clear();
+Console.WriteLine("zadejte  druhé číslo");
+string cislo2 = Console.ReadLine();
+int cislo22 = Convert.ToInt32(cislo2);
+Console.Clear();
+
+if (cislo11 < cislo22)
+    Console.WriteLine($"{cislo11} je menší než {cislo22}");
+
+if (cislo11 > cislo22)
+    Console.WriteLine($"{cislo22} je menší než  {cislo11}");
